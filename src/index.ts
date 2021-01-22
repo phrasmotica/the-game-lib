aa7c0eacb672dd4204ce831a0fa9a849fea0332d
Julian Lawrance <julian.lawrance@gmail.com>
2021-01-22 20:59:47 +0000

    Export Card and MulliganResult from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,25 @@
+import { Card } from "./game/Card"
 import { Deck } from "./game/Deck"
+import { GameData, GameStartResult, PlayerHandMap } from "./game/GameData"
 import { Hand } from "./game/Hand"
 import { Direction, Pile, PileState } from "./game/Pile"
 import { IRuleSet, RuleSet } from "./game/RuleSet"
 
-import { GameData, GameStartResult, PlayerHandMap } from "./game/GameData"
+import { MulliganResult } from "./game/results/MulliganResult"
 
 import { IVoteCalculator } from "./voting/IVoteCalculator"
 import { UnanimousVoteCalculator } from "./voting/UnanimousVoteCalculator"
 import { Vote, VoteCalculationMethod, VoteResult } from "./voting/Vote"
 
 export {
+    Card,
     Deck,
+    GameData, GameStartResult, PlayerHandMap,
     Hand,
     Direction, Pile, PileState,
     IRuleSet, RuleSet,
 
-    GameData, GameStartResult, PlayerHandMap,
+    MulliganResult,
 
     IVoteCalculator,
     UnanimousVoteCalculator,
