6f14af4a717149bb2801313d676ad3e8d9871d8d
Julian Lawrance <julian.lawrance@gmail.com>
2021-01-23 12:13:15 +0000

    Export more types from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,30 @@ import { Card } from "./game/Card"
 import { Deck } from "./game/Deck"
 import { GameData, GameStartResult, PlayerHandMap } from "./game/GameData"
 import { Hand } from "./game/Hand"
-import { Direction, Pile, PileState } from "./game/Pile"
-import { IRuleSet, RuleSet } from "./game/RuleSet"
+import { CardInfo, Direction, Pile, PileState } from "./game/Pile"
+import { GameMode, IRuleSet, RuleSet, RuleSetBuilder } from "./game/RuleSet"
 
 import { MulliganResult } from "./game/results/MulliganResult"
 
+import { Random } from "./util/Random"
+
 import { IVoteCalculator } from "./voting/IVoteCalculator"
 import { UnanimousVoteCalculator } from "./voting/UnanimousVoteCalculator"
-import { Vote, VoteCalculationMethod, VoteResult } from "./voting/Vote"
+import { Vote, VoteCalculationMethod, VoteMap, VoteResult } from "./voting/Vote"
 
 export {
     Card,
     Deck,
     GameData, GameStartResult, PlayerHandMap,
     Hand,
-    Direction, Pile, PileState,
-    IRuleSet, RuleSet,
+    CardInfo, Direction, Pile, PileState,
+    GameMode, IRuleSet, RuleSet, RuleSetBuilder,
 
     MulliganResult,
 
+    Random,
+
     IVoteCalculator,
     UnanimousVoteCalculator,
-    Vote, VoteCalculationMethod, VoteResult,
+    Vote, VoteCalculationMethod, VoteMap, VoteResult,
 }
